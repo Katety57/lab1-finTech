769b895a33bf4d76d1e051dcdced1dcdcb15f1e9
User <user@MacBook-Air-User.local>
2020-03-01 22:52:59 +0300

    hw3

--- TinkoffChat/ViewController.swift
+++ TinkoffChat/ViewController.swift
@@ -6,61 +6,83 @@
 //  Copyright © 2020 Tinkoff Bank. All rights reserved.
 //
 
+
+// lets build that app - youtube.com
 import UIKit
 
-class ViewController: UIViewController {
-    let delegate = UIApplication.shared.delegate as! AppDelegate
-    var turnOffLog: Bool?
-
+final class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var cameraButton: UIButton!
+    @IBOutlet weak var textDescript: UILabel!
+    @IBOutlet weak var editButton: UIButton!
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        profileImg.layer.cornerRadius =  cameraButton.frame.width/2.0
+        
+        cameraButton.layer.cornerRadius = cameraButton.frame.width/2.0
+        
+        textDescript.textColor = UIColor.gray
 
-        turnOffLog = delegate.turnOffLog
-        // Do any additional setup after loading the view.
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        if turnOffLog == false {
-            print("View is from <Disappeared> or <Disappearing> to <Appearing>: \(#function)\n")
-        }
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        if turnOffLog == false {
-            print("View is from <Appearing> to <Appeared>: \(#function)\n")
-        }
-    }
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        if turnOffLog == false {
-            print("View is from <Appearing> to <Appeared>, goes between <viewWillAppear> and <viewDidAppear>: \(#function)\n")
-        }
+        editButton.layer.borderWidth = 2
+        editButton.layer.borderColor = UIColor.black.cgColor
+        editButton.layer.cornerRadius = 10
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        if turnOffLog == false {
-            print("View is from <Appearing> to <Appeared>, goes between <viewWillAppear> and <viewDidAppear>: \(#function)\n")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    func camera() {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+
+    }
+    
+    func photoLibrary() {
+        let myPickerController = UIImagePickerController()
+        myPickerController.delegate = self
+        myPickerController.sourceType = UIImagePickerController.SourceType.photoLibrary
+        myPickerController.allowsEditing = false
+        self.present(myPickerController, animated: true, completion: nil)
+
+    }
+    
+    @IBAction func chooseImg(_ sender: UIButton) {
+        if sender.tag == 1 {
+            let ac = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            ac.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: {(alert:UIAlertAction!) -> Void in self.camera()}))
+            ac.addAction(UIAlertAction(title: "Choose Photo", style: .default, handler: {(alert:UIAlertAction!) -> Void in self.photoLibrary()}))
+            present(ac, animated: true, completion: nil)
+            ac.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil))
+
         }
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        if turnOffLog == false {
-            print("View is from <Appearing> or <Appeared> to <Disappearing>: \(#function)\n")
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            profileImg.contentMode = .scaleAspectFill
+            profileImg.image = pickedImage
         }
+        picker.dismiss(animated: true, completion: nil)
     }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        if turnOffLog == false {
-            print("View is from <Disappearing> to <Disappeared>: \(#function)\n")
-        }
-    }
-
-
 }
 
